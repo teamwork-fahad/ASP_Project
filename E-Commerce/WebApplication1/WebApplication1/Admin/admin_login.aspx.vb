59455e53c7c225a745e6eac8f01cad37780570a4
Fahad Vahora <fahadvohra143@gmail.com>
2024-08-03 18:01:54 +0530

    changes in E-Commerce

--- E-Commerce/WebApplication1/WebApplication1/Admin/admin_login.aspx.vb
+++ E-Commerce/WebApplication1/WebApplication1/Admin/admin_login.aspx.vb
@@ -1,8 +1,14 @@
-﻿Public Class admin_login
+﻿Imports System.Configuration
+Imports System.Data
+Imports System.Data.SqlClient
+Public Class admin_login
     Inherits System.Web.UI.Page
-
+    Dim conn As String
+    Dim myConn As SqlConnection
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        conn = ConfigurationManager.ConnectionStrings("conn").ConnectionString
+        myConn = New SqlConnection(conn)
+        Response.Write(myConn.State)
     End Sub
 
 End Class
